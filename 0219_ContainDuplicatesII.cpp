0a84503c4eb3f18e6f7ed831388631f859369079
Anurag2575 <anuragdeshmukh2575@gmail.com>
2025-04-10 01:00:44 +0530

    219. Contain Duplicates II

---
+++ 0219_ContainDuplicatesII.cpp
@@ -0,0 +1,12 @@
+#include<unordered_map>
+#include<vector>
+using namespace std;
+
+bool containDuplicates(vector<int>& nums, int k){
+    unordered_map<int, int> lastSeen;
+    for(int i = 0; i < nums.size(); i++){
+        if(lastSeen.count(nums[i]) && i - lastSeen[nums[i]] <= k)   return true;
+        lastSeen[nums[i]] = i;
+    }
+    return false;
+}
