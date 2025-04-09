2b47b5fadabd5e75aaf3b8cbd2348b3010ea1505
Anurag2575 <anuragdeshmukh2575@gmail.com>
2025-04-09 23:47:28 +0530

    1. Two Sum

--- 0001_TwoSum.cpp
+++ 0001_TwoSum.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 vector<int> twoSum(vector<int>& nums, int target){
   unordered_map<int, int> numbers; //map to store number and its index
+  
   for(int i = 0; i < nums.size(); i++){
     int moreNeeded = target - nums[i];
     if(numbers.find(moreNeeded) != numbers.end()) return {numbers[moreNeeded], i};
